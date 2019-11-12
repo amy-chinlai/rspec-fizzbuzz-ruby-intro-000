a2c95f357f44ccdff7c0715180c8cb96e83284f4
Amy Chin-Lai <amy.chinlai@gmail.com>
2019-11-12 19:04:20 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,10 @@
 def fizzbuzz(int)
   if int % 15 == 0
-    puts "FizzBuzz"
+     "FizzBuzz"
   elsif int % 3 == 0
-    puts "Fizz"
+     "Fizz"
   elsif int % 15 == 0
-    puts "Buzz"
+     "Buzz"
   end
 end# Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
