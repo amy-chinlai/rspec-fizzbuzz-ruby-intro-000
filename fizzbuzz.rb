7afb22b5430bdb2faccc1e240e186d07f5dc48c7
Amy Chin-Lai <amy.chinlai@gmail.com>
2019-11-12 19:03:46 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -6,4 +6,5 @@ def fizzbuzz(int)
   elsif int % 15 == 0
     puts "Buzz"
   end
-end
+end# Don't forget! This file needs to be 'required' in its spec file
+# See README.md for instructions on how to do this
